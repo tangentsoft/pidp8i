6d0c2c8a5094b5f9208cb4f8c0da5daaf6aa3cc1
tangent <pidp8i@tangentsoft.com>
2017-01-23 03:32:29 +0000

    Flipped this branch back to integer math for others to play with, even though we've abandoned the idea.  We're using somewhat different constants here based on experimentation done in the meantime.
    FossilOrigin-Name: a9940bb04ff5c0dca5df49c7caf0e849be6be4ab

--- src/gpio-ils.c
+++ src/gpio-ils.c
@@ -37,15 +37,24 @@
 
 //// CONSTANTS /////////////////////////////////////////////////////////
 
-// Brightness range is [0, MAX_BRIGHTNESS) truncated.
-#define MAX_BRIGHTNESS 32
+// Brightness range is [0, BRIGHTNESS_LEVELS) truncated.  We scale that
+// up to [0, SCALED_MAX_BRIGHTNESS) for the decay calculation and then
+// shift it right by log2(SCALED_MAX_BRIGHTNESS + 1) - 
+// log2(BRIGHTNESS_LEVELS) bits to convert it to a brightness level.
+#define BRIGHTNESS_LEVELS 32
+#define SCALED_MAX_BRIGHTNESS ((uint16)~0)
+#define BRIGHTNESS_CONV_SHIFT 11
 
 // On each iteration, we add or subtract a proportion of the current LED
 // value back to it as its new brightness, based on the LED's current
 // internal state.  This gives a nonlinear increase/decrease behavior,
 // where rising from "off" or dropping from "full-on" is fast to start
 // and slows down as it approaches its destination.
-#define DECAY 0.01
+//
+// For this integer approximation, we need to use an asymmetric function
+// depending on whether it's rising or falling.
+#define RISING_SHIFT 5
+#define FALLING_SHIFT 1
 
 
 //// blink_core ////////////////////////////////////////////////////////
@@ -58,7 +67,7 @@ void blink_core(struct bcm2835_peripheral* pgpio, int* terminate)
     const us_time_t intervl = 5;    // light each row of leds 5 µs
     ms_time_t now_ms;
 
-	float brtval[96];
+	uint16 brtval[96];
 	uint8 brctr[96], bctr = 0, ndx;
 	memset(brtval, 0, sizeof (brtval));
 
@@ -70,20 +79,20 @@ void blink_core(struct bcm2835_peripheral* pgpio, int* terminate)
         }
         if (bctr == 0) {
 			memset(brctr, 0, sizeof (brctr));
-            bctr = MAX_BRIGHTNESS;
+            bctr = BRIGHTNESS_LEVELS;
         }
 
 		// Increase or decrease each LED's brightness based on whether
 		// it is currently enabled.  These values affect the duty cycle
 		// of the signal put out by the GPIO thread to each LED, thus
 		// controlling brightness.
-		float *p = brtval;
+		uint16 *p = brtval;
 		for (int row = 0; row < nledrows; ++row) {
 			for (int col = 0, msk = 1; col < ncols; ++col, ++p, msk <<= 1) {
 				if (ledstatus[row] & msk)
-					*p += (MAX_BRIGHTNESS - *p) * DECAY;
+					*p += (SCALED_MAX_BRIGHTNESS - *p) >> RISING_SHIFT;
 				else
-					*p -= *p * DECAY;
+					*p -= *p >> FALLING_SHIFT;
 			}
 		}
 
@@ -91,7 +100,7 @@ void blink_core(struct bcm2835_peripheral* pgpio, int* terminate)
         for (i = ndx = 0; i < nledrows; i++) {
             // Toggle columns for this ledrow (which LEDs should be on (CLR = on))
             for (k = 0; k < ncols; k++, ndx++) {
-                if (++brctr[ndx] < brtval[ndx])
+                if (++brctr[ndx] < (brtval[ndx] >> BRIGHTNESS_CONV_SHIFT))
                     GPIO_CLR = 1 << cols[k];
                 else
                     GPIO_SET = 1 << cols[k];
