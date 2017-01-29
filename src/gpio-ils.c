5ccff01a593e4af404799d846f0d9b5713d3853f
tangent <pidp8i@tangentsoft.com>
2017-01-29 08:18:23 +0000

    ILS version now mostly works with the new LED "on" count mechanism. It's a little flickery, but it takes a *lot* less CPU than before!
    FossilOrigin-Name: 8ac46a0ea84bd99e6cb8c8b14e180ae3493ad882

--- src/gpio-ils.c
+++ src/gpio-ils.c
@@ -36,8 +36,6 @@
 
 #include "PDP8/pidp8i.h"
 
-#include <math.h>
-
 
 //// CONSTANTS /////////////////////////////////////////////////////////
 
@@ -53,18 +51,8 @@
 // We use an asymmetric function depending on whether the LED is turning
 // on or off to better mimic the behavior of an incandescent lamp, which
 // reaches full brightness faster than it turns fully off.
-#define RISING_FACTOR 0.2
-#define FALLING_FACTOR 0.08
-
-// If the CPU core is executing a static mix of instructions, the actual
-// brightness will eventually get close enough to the ideal brightness
-// that there's no point continuing to adjust the value because we'll
-// just end up oscillating around the ideal value.  If that oscillation
-// happens to cross one of the brightness quantization thresholds, the
-// LED will pulse when it should be rock-steady.
-//
-// Therefore, once we're within half of a brightness step, leave it be.
-#define BRIGHTNESS_EPSILON (1.0 / MAX_BRIGHTNESS / 2.0)
+#define RISING_FACTOR 0.05
+#define FALLING_FACTOR 0.02
 
 
 //// blink_core ////////////////////////////////////////////////////////
@@ -80,45 +68,66 @@ void blink_core(struct bcm2835_peripheral* pgpio, int* terminate)
     // is about the same for both versions.
     const us_time_t intervl = 5;
 
-    float brtval[96];
-    uint8 bctr = MAX_BRIGHTNESS;
-    memset(brtval, 0, sizeof (brtval));
+	// Current brightness level for each LED.  It goes from 0 to
+	// MAX_BRIGHTNESS, but we keep it as a float because the decay
+	// function smoothly ramps from the current value to the ever-
+	// changing target value.
+    float brightness[NLEDROWS][NCOLS];
+    memset(brightness, 0, sizeof (brightness));
+
+	// Brightness target for each LED, updated at the start of each PWM
+	// cycle when we get fresh "on" counts from the CPU thread.
+	uint8 br_targets[NLEDROWS][NCOLS];
+    memset(br_targets, 0, sizeof (br_targets));
+
+	// Current PWM brightness step
+    uint8 step = MAX_BRIGHTNESS;
 
     while (*terminate == 0) {
         // Prepare for lighting LEDs by setting col pins to output
         for (size_t i = 0; i < NCOLS; ++i) OUT_GPIO(cols[i]);
 
         // Restart PWM cycle if prior one is complete
-        if (bctr == MAX_BRIGHTNESS) {
+        if (step == MAX_BRIGHTNESS) {
             // Reset PWM step counter
-            bctr = 0;
+            step = 0;
       
             // Go get the current LED "on" times, and give the SIMH
-            // CPU thread a blank copy to begin updating.  We save the
-            // instructions-executed count as a float to avoid a cast in
-            // the calculation that uses it, where we want an FP result.
-            const float inst_count = swap_displays();
+            // CPU thread a blank copy to begin updating.
+            const size_t inst_count = swap_displays();
 
-            // Update the brightness values used in the next PWM cycle.
-            // Increase the LED's brightness where the target brightness
-            // is greater than the actual brightness, else decrease it.
-            float *p = brtval;
-            for (int row = 0; row < NLEDROWS; ++row) {
-                size_t *prow = pdis_paint->on[row];
-                for (int col = 0; col < NCOLS; ++col, ++p) {
-                    float new_brtval = prow[col] / inst_count;
-                    if (fabs(new_brtval - *p) > BRIGHTNESS_EPSILON) {
-                        if (new_brtval > *p) {
-                            *p += (MAX_BRIGHTNESS - *p) * RISING_FACTOR;
-                        }
-                        else {
-                            *p -= *p * FALLING_FACTOR;
-                        }
-                    }
-                }
-            }
+            // Recalculate the brightness target values based on the
+			// "on" counts in *pdis_paint and the quantized brightness
+			// level, which is based on the number of instructions
+			// executed for this display update.
+            //
+            // Handle the cases where inst_count is <= 1 specially
+            // because we don't want all LEDs to go out when the
+            // simulator is heavily throttled.
+			size_t br_quant = inst_count > 1 ? (inst_count >> 5) : 1;
+			for (int row = 0; row < NLEDROWS; ++row) {
+				size_t *prow = pdis_paint->on[row];
+				for (int col = 0; col < NCOLS; ++col) {
+					br_targets[row][col] = prow[col] / br_quant;
+				}
+			}
         }
-        ++bctr;
+        ++step;
+
+		// Update the brightness values.
+		for (int row = 0; row < NLEDROWS; ++row) {
+			size_t *prow = pdis_paint->on[row];
+			for (int col = 0; col < NCOLS; ++col) {
+				uint8 br_target = br_targets[row][col];
+				float *p = brightness[row] + col;
+				if (*p <= br_target) {
+					*p += (br_target - *p) * RISING_FACTOR;
+				}
+				else {
+					*p -= *p * FALLING_FACTOR;
+				}
+			}
+		}
 
         // Halve the Execute and Fetch values, because they're only on
         // for half of the instruction fetch-and-execute cycle.  We have
@@ -128,11 +137,11 @@ void blink_core(struct bcm2835_peripheral* pgpio, int* terminate)
         float fetch   = pdis_paint->on[5][3] / 2.0;
 
         // Light up LEDs
-        for (size_t row = 0, ndx = 0; row < NLEDROWS; ++row) {
+        for (size_t row = 0; row < NLEDROWS; ++row) {
             // Output 0 (CLR) for LEDs in this row which should be on
             size_t *prow = pdis_paint->on[row];
-            for (size_t col = 0; col < NCOLS; ++col, ++ndx) {
-                if (brtval[ndx] >= bctr) {
+            for (size_t col = 0; col < NCOLS; ++col) {
+                if (brightness[row][col] >= step) {
                     GPIO_CLR = 1 << cols[col];
                 }
                 else {
