783a38f1ae6cd4938d4b63a3466a282207a20375
tangent <pidp8i@tangentsoft.com>
2017-09-30 17:16:25 +0000

    Reorganized the mkos8/argparser component stuff into a subdirectory structure so that we can import the whole subtree with an "import *" call, so that the upcoming modules don't all need to be named explicitly.
    FossilOrigin-Name: 73cf71943efccf0b354edaba55558b18063ff919

--- lib/mkos8/argparser/__init__.py
+++ lib/mkos8/argparser/__init__.py
@@ -3,7 +3,7 @@
 ########################################################################
 # argparse.py - Extend ArgumentParser to add mkos8 bits.
 #
-# Copyright © 2017 by Warren Young.
+# Copyright © 2017 by Warren Young
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
