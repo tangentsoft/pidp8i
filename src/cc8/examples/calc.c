898d71534fe19be47c8968480401379cb03765dc
tangent <pidp8i@tangentsoft.com>
2017-11-16 20:01:42 +0000

    Removed the test for lowercase 'y' in the reworked CC8 example calc.c: CC8 doesn't yet understand the C || operator.  None of my other attempts to work around this work, either.  (cupper, restructuring as nested if/elseif/else tests, etc.)
    FossilOrigin-Name: 156b2b69e905d1a3f1b60993a94d6de1c6f49b08

--- src/cc8/examples/calc.c
+++ src/cc8/examples/calc.c
@@ -1,47 +1,45 @@
 int main()
 {
-  int x, y, ans;
-  int choice;
-  int bfr[10];
+    int x, y, ans;
+    int choice;
+    int bfr[10];
 
-  ans = 'Y';
+    ans = 'Y';
 
-  while (1)
-  {
-    if (ans == 'Y' || ans == 'y')
+    while (1)
     {
-      printf ("\r\nENTER ANY TWO NUMBERS.\r\n");
+        if (ans == 'Y') {
+            printf("\r\nENTER ANY TWO NUMBERS.\r\n");
 
-      printf ("  ENTER THE FIRST NUMBER: ");
-      gets (bfr);
-      sscanf (bfr, "%d", &x);
+            printf("ENTER THE FIRST NUMBER: ");
+            gets(bfr);
+            sscanf(bfr, "%d", &x);
 
-      printf ("  ENTER THE SECOND NUMBER: ");
-      gets (bfr);
-      sscanf (bfr, "%d", &y);
+            printf("ENTER THE SECOND NUMBER: ");
+            gets(bfr);
+            sscanf(bfr, "%d", &y);
 
-      printf ("SELECT THE OPERATION:\r\n");
+            printf("SELECT THE OPERATION:\r\n");
 
-      printf ("1: ADDITION\r\n");
-      printf ("2: SUBTRACTION\r\n");
-      printf ("3: MULTIPLICATION\r\n");
-      printf ("4: DIVISION\r\n");
+            printf("1: ADDITION\r\n");
+            printf("2: SUBTRACTION\r\n");
+            printf("3: MULTIPLICATION\r\n");
+            printf("4: DIVISION\r\n");
 
-      printf ("CHOICE:");
-      gets (bfr);
-      sscanf (bfr, "%d", &choice);
+            printf("CHOICE: ");
+            gets(bfr);
+            sscanf(bfr, "%d", &choice);
 
-      if (choice == 1) printf ("Result: %d\r\n", x + y);
-      if (choice == 2) printf ("Result: %d\r\n", x - y);
-      if (choice == 3) printf ("Result: %d\r\n", x * y);
-      if (choice == 4) printf ("Result: %d\r\n", x / y);
+            if (choice == 1) printf("Result: %d\r\n", x + y);
+            if (choice == 2) printf("Result: %d\r\n", x - y);
+            if (choice == 3) printf("Result: %d\r\n", x * y);
+            if (choice == 4) printf("Result: %d\r\n", x / y);
 
-      printf ("DO YOU WISH TO CONTINUE (Y/N): ");
-      ans = getc ();
+            printf("DO YOU WISH TO CONTINUE (Y/N): ");
+            ans = getc();
+        }
+        else {
+            break;
+        }
     }
-    else
-    {
-      break;
-    }
-  }
 }
