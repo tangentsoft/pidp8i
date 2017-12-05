5937c50886d04b5fa730f7d513adfde24525dffc
tangent <pidp8i@tangentsoft.com>
2017-12-05 12:53:51 +0000

    Simplified pep001.c a bit more.
    FossilOrigin-Name: daf2ff90901468455e303e42d310a7c01a127736

--- examples/pep001.c
+++ examples/pep001.c
@@ -1,9 +1,6 @@
 ire0 (n, d)
-int n, d;
 {
-    while (n > 0) {
-        n = n - d;
-    }
+    while (n > 0) n = n - d;
     return n == 0;
 }
 
@@ -13,12 +10,8 @@ main ()
     st = 0;
 
     for (i = 3; i < 1000; i++) {
-        if (ire0 (i, 3)) {
-            st = st + i;
-        }
-        else if (ire0 (i, 5)) {
-            st = st + i;
-        }
+             if (ire0 (i, 3)) st = st + i;
+        else if (ire0 (i, 5)) st = st + i;
 
         if (st > 1000) {
             printf("%d + ", st);
