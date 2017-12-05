1a4723ef59ad634d3b43e432b60e04e2c7ca8730
tangent <pidp8i@tangentsoft.com>
2017-12-05 13:07:43 +0000

    Reworked pep001.c in terms of the new modulus operator support.  (The only reason I checked in the previous ire0() based versions is because they're used to tell the story in the PEP001.C wiki article I'm composing, and I'd like to be able to pull those versions back out of source control again later.)
    FossilOrigin-Name: 199a0dff7e1c06c5e06be856250f98b88e4179c1

--- examples/pep001.c
+++ examples/pep001.c
@@ -1,17 +1,11 @@
-ire0 (n, d)
-{
-    while (n > 0) n = n - d;
-    return n == 0;
-}
-
 main ()
 {
     int i, st;
     st = 0;
 
     for (i = 3; i < 1000; i++) {
-             if (ire0 (i, 3)) st = st + i;
-        else if (ire0 (i, 5)) st = st + i;
+             if (i % 3 == 0) st = st + i;
+        else if (i % 5 == 0) st = st + i;
 
         if (st > 1000) {
             printf("%d + ", st);
