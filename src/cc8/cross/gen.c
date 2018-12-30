50b26a9551ae90cabf7f5337c0f149b06d942325
poetnerd <bill.cattey@gmail.com>
2018-12-30 16:51:30 +0000

    New code base for cc8/cross: SmallC-85. SmallC-85 is a revived and updated port of SmallC that is much cleaner. This version compiles on the Mac using clang silently without warnings, and runs well enough to generate .sb files that are functionally the same as what was generated on the non-portable cross. Carefully uses today's trunk as baseline.
    FossilOrigin-Name: 385a16c7ccf01365b1e27a5e368741e718027e36

--- src/cc8/cross/gen.c
+++ src/cc8/cross/gen.c
@@ -1,4 +1,4 @@
-/*	File gen.c: 2.1 (83/03/20,16:02:06) */
+/*      File gen.c: 2.1 (83/03/20,16:02:06) */
 /*% cc -O -c %
  *
  */
@@ -6,166 +6,197 @@
 #include <stdio.h>
 #include "defs.h"
 #include "data.h"
+#include "extern.h"
 
 /* ToUpper routine */
 
-	ucase(ch)
-	int ch;
-	{
-		if ('a'>ch || ch>'z') return(ch);
-		return(ch-32);
-	}
-/*
- *	return next available internal label number
- *
+ucase(int ch) {
+  if ('a'>ch || ch>'z') return(ch);
+  return(ch-32);
+}
+
+/**
+ * return next available internal label number
  */
-getlabel ()
-{
-	return (nxtlab++);
+int getlabel() {
+    return (nxtlab++);
 }
 
-/*
- *	print specified number as label
+/**
+ * print specified number as label
+ * @param label
  */
-printlabel (label)
-int	label;
-{
-	olprfix ();
-	outdec (label);
-    return 0;
+void print_label(int label) {
+    output_label_prefix ();
+    output_decimal (label);
 }
 
-/*
- *	glabel - generate label
+/**
+ * glabel - generate label
+ * not used ?
+ * @param lab label number
  */
-glabel (lab)
-char	*lab;
-{
-	prefix ();
-	outstr (lab);
-	col ();
-	nl ();
-    return 0;
+void glabel(char *lab) {
+    output_string (lab);
+    output_label_terminator ();
+    newline ();
 }
 
-/*
- *	gnlabel - generate numeric label
+/**
+ * generate_label - generate numeric label
+ * @param nlab label number
+ * @return 
  */
-gnlabel (nlab)
-int	nlab;
-{
-	printlabel (nlab);
-	col ();
-	nl ();
-    return 0;
+void generate_label(int nlab) {
+    print_label (nlab);
+    output_label_terminator ();
+    newline ();
 }
 
-outbyte (c)
-char	c;
-{
-	if (c == 0)
-		return (0);
-	fputc (c, output);
-	return (c);
+/**
+ * outputs one byte
+ * @param c
+ * @return 
+ */
+char output_byte(char c) {
+    if (c == 0)
+        return (0);
+    fputc (c, output);
+    return (c);
 }
 
-outstr (ptr)
-char	ptr[];
-{
-	int	k;
-
-	k = 0;
-	while (outbyte (ucase(ptr[k++])));
-    return 0;
+/**
+ * outputs a string
+ * @param ptr the string
+ * @return 
+ */
+void output_string(char ptr[]) {
+    int k;
+    k = 0;
+    while (output_byte (ucase(ptr[k++])));
+    if (0) printf ("DBG: %s\n", ptr);
 }
 
-
-tab ()
-{
-	outbyte (9);
-    return 0;
+/**
+ * outputs a tab
+ * @return 
+ */
+void print_tab() {
+    output_byte ('\t');
 }
 
-ol (ptr)
-char	ptr[];
+/**
+ * output line
+ * @param ptr
+ * @return 
+ */
+void output_line(char ptr[])
 {
-	ot (ptr);
-	nl ();
-    return 0;
+    output_with_tab (ptr);
+    newline ();
 }
 
-ot (ptr)
-char	ptr[];
-{
-	outstr (ptr);
-    return 0;
+/**
+ * tabbed output
+ * @param ptr
+ * @return 
+ */
+void output_with_tab(char ptr[]) {
+    // print_tab ();
+    output_string (ptr);
 }
 
-outdec (number)
-int	number;
-{
+/**
+ * output decimal number
+ * @param number
+ * @return 
+ */
+void output_decimal(int number) {
 	int	k, zs;
 	char	c;
 
 	if (number == -32768) {
-		outstr ("-32768");
-		return 0;
+		output_string ("-32768");
+		return;
 	}
 	zs = 0;
 	k = 10000;
 	if (number < 0) {
 		number = (-number);
-		outbyte ('-');
+		output_byte ('-');
 	}
 	while (k >= 1) {
 		c = number / k + '0';
 		if ((c != '0' | (k == 1) | zs)) {
 			zs = 1;
-			outbyte (c);
+			output_byte (c);
 		}
 		number = number % k;
 		k = k / 10;
 	}
-    return 0;
-}
-		
-store (lval)
-int	*lval;
-{
-	if (lval[1] == 0)
-		putmem (lval[0]);
-	else
-		putstk (lval[1]);
-    return 0;
+    return;
 }
 
-rvalue (lval)
-int	*lval;
-{
-	if ((lval[0] != 0) & (lval[1] == 0))
-		getmem (lval[0]);
-	else
-		indirect (lval[1]);
-    return 0;
+/**
+ * stores values into memory
+ * @param lval
+ * @return 
+ * Why does store use stack for indirect, and get use indirect?
+ */
+void store(LVALUE *lval) {
+    if (lval->indirect == 0)
+        gen_put_memory (lval->symbol);
+    else
+        gen_put_stack (lval->indirect);
 }
 
-rivalue (lval)
-int	*lval;
-{
-	if ((lval[0] != 0) & (lval[1] == 0))
-		getincmem (lval[0]);
-	else
-		incdirect (lval[1]);
-    return 0;
+int rvalue(LVALUE *lval, int reg) {
+    if ((lval->symbol != 0) & (lval->indirect == 0))
+        gen_get_memory (lval->symbol);
+    else
+        gen_get_indirect (lval->indirect, reg);
+    return HL_REG;
 }
 
-test (label, ft)
-int	label,
-	ft;
-{
-	needbrack ("(");
-	expression (YES);
-	needbrack (")");
-	testjump (label, ft);
-    return 0;
+int rivalue(LVALUE *lval, int reg) {
+    if ((lval->symbol != 0) & (lval->indirect == 0))
+        gen_get_inc_memory (lval->symbol);
+    else
+        gen_inc_direct (lval->indirect, reg);
+    return HL_REG;
+}
+
+
+/**
+ * parses test part "(expression)" input and generates assembly for jump
+ * @param label
+ * @param ft : false - test jz, true test jnz
+ * @return 
+ */
+void test(int label, int ft) {
+    needbrack ("(");
+    expression (YES);
+    needbrack (")");
+    gen_test_jump (label, ft);
+}
+
+/**
+ * scale constant depending on type
+ * @param type
+ * @param otag
+ * @param size
+ * @return 
+ */
+void scale_const(int type, int otag, int *size) {
+    switch (type) {
+        case CINT:
+        case UINT:
+            *size += *size;
+            break;
+        case STRUCT:
+            *size *= tag_table[otag].size;
+            break;
+        default:
+            break;
+    }
 }
