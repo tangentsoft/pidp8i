621eac1455b58dea4fed7985b0943d42a8e873be
tangent <pidp8i@tangentsoft.com>
2019-03-11 00:56:37 +0000

    Checkin [a96f0c5f20] missed a location where the old, incorrect "MQO" operator definition in the OS/8 CC8's code generator wasn't changed to the corrected mnemonic, MQA.  Bug reported by Ian Schofield.
    FossilOrigin-Name: 9074f0ba54f344cf3b27df701340bc9c2e4e90e2ca8d2a85cb04fc345bc2e154

--- src/cc8/os8/p8.c
+++ src/cc8/os8/p8.c
@@ -160,7 +160,7 @@ main()
 				fputs("\tANDI STKP\r\n\tJMSI POP\r\n");
 				break;
 			case -20:
-				fputs("\tJMSI POP\r\n\tMQO\r\n");
+				fputs("\tJMSI POP\r\n\tMQA\r\n");
 				break;
 			case 21:
 				if (strl()>0) 
